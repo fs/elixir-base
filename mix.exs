9235c418ace9bae09a1d05739df52adc22a9400c
AlexeyKuznetzov <alexey.kuznetzov@flatstack.com>
2016-06-20 13:24:31 +0300

    Set mod in configuration

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule ElixirBase.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger], mod: {ElixirBase, []}]
   end
 
   # Dependencies can be Hex packages:
